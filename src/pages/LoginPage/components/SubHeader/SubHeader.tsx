5edf0fd6173f7258fe6989634f8a1b154c06ca07
DimaKertpich <dimavoluychik@gmail.com>
2024-07-01 17:08:47 +0300

    add subheader sections text

--- src/pages/LoginPage/components/SubHeader/SubHeader.tsx
+++ src/pages/LoginPage/components/SubHeader/SubHeader.tsx
@@ -1,4 +1,4 @@
-import { Stack, Box } from "@mui/material"
+import { Stack, Box, Typography } from "@mui/material"
 import noveltyImg from '../../../../shared/styles/img/1.jpg';
 import forHimImg from '../../../../shared/styles/img/2.jpg';
 import discounts from '../../../../shared/styles/img/3.jpg';
@@ -10,15 +10,15 @@ const SubHeader = () => {
         <Stack className="subHeader">
             <Box className="subHeaderBoxImg subHeaderBoxImgLeft">
                 <img className="subHeaderImg" src={noveltyImg} alt='img1'></img>
-                <div className="subHeaderBoxImgBefore"></div>
+                <div className="subHeaderBoxImgBefore"><Typography sx={{left: '30% !important'}}>Знижки</Typography></div>
             </Box>
             <Box className="subHeaderBoxImg subHeaderBoxImgCenter">
-                <img className="subHeaderImg" src={forHimImg} alt='img1'></img>
-                <div className="subHeaderBoxImgBefore"></div>
+                <img className="subHeaderImg" src={forHimImg} alt='img2'></img>
+                <div className="subHeaderBoxImgBefore"><Typography>Для нього</Typography></div>
             </Box>
             <Box className="subHeaderBoxImg subHeaderBoxImgRight">
-                <img className="subHeaderImg" src={discounts} alt='img1'></img>
-                <div className="subHeaderBoxImgBefore"></div>
+                <img className="subHeaderImg" src={discounts} alt='img3'></img>
+                <div className="subHeaderBoxImgBefore"><Typography>Новинки</Typography></div>
             </Box>
         </Stack>
     )
