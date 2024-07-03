a3576141093581156e432a838b5a9e0d9483d7c9
David600 <david.kardash@egym.com>
2024-07-03 17:24:36 +0300

    small update in keys

--- src/pages/MainPage/Components/SubHeader/SubHeader.tsx
+++ src/pages/MainPage/Components/SubHeader/SubHeader.tsx
@@ -9,17 +9,23 @@ const SubHeader = () => {
   const { t } = useTranslation();
   return (
     <Stack className="subHeader">
-      <Box className="subHeaderBoxImg" sx={{zIndex: '10'}}>
+      <Box className="subHeaderBoxImg" sx={{ zIndex: "10" }}>
         <img className="subHeaderImg" src={noveltyImg} alt="img1"></img>
-        <Box className="subHeaderBoxBefore"><Typography>{t('header.discount')}</Typography></Box>
+        <Box className="subHeaderBoxBefore">
+          <Typography>{t("header.discount")}</Typography>
+        </Box>
       </Box>
-      <Box className="subHeaderBoxImg" sx={{zIndex: '5'}}>
+      <Box className="subHeaderBoxImg" sx={{ zIndex: "5" }}>
         <img className="subHeaderImg" src={forHimImg} alt="img2"></img>
-        <Box className="subHeaderBoxBefore"><Typography>{t('header.forHim')}</Typography></Box>
+        <Box className="subHeaderBoxBefore">
+          <Typography>{t("header.forHim")}</Typography>
+        </Box>
       </Box>
-      <Box className="subHeaderBoxImg" sx={{zIndex: '1'}}>
+      <Box className="subHeaderBoxImg" sx={{ zIndex: "1" }}>
         <img className="subHeaderImg" src={discounts} alt="img3"></img>
-        <Box className="subHeaderBoxBefore"><Typography>{t('header.novelties')}</Typography></Box>
+        <Box className="subHeaderBoxBefore">
+          <Typography>{t("header.new")}</Typography>
+        </Box>
       </Box>
     </Stack>
   );
